94999c015db845ce8c679ce5f7648cd5f836efb3
Brendan Murty <brendan@bcm.works>
2026-07-12 19:09:37 +1000

    Improve Git setup scripts

--- dev/git/git-bash-setup.sh
+++ dev/git/git-bash-setup.sh
@@ -11,26 +11,32 @@ source "$REPO/bin/.helper.sh"
 OS="$(os)"
 
 if [[ "$OS" == "Ubuntu" ]]; then
-  echo 'Ubuntu: Install Git and Curl'
-  sudo apt update -qq
-  sudo apt -qq --assume-yes install git curl
+  info 'Ubuntu: Install Git and Curl'
+  sudo apt update -qq > /dev/null 2>&1
+  sudo apt -qq --assume-yes install git curl > /dev/null 2>&1
 fi
 
-echo 'Download the Git Prompt script from the official Git Repo to ~/.git_bash_prompt.sh'
-echo 'When in a Git directory, this gets details about the status of the Repo.'
+info 'Download the Git Prompt script from the official Git Repo to ~/.git_bash_prompt.sh'
+info 'When in a Git directory, this gets details about the status of the Repo.'
+
+if [ -f "$HOME/.git_bash_prompt.sh" ]; then
+	mv "$HOME/.git_bash_prompt.sh" "$HOME/.git_bash_prompt.sh.old"
+fi
 
 touch "$HOME/.git_bash_prompt.sh"
-cp "$HOME/.git_bash_prompt.sh" "$HOME/.git_bash_prompt.sh.old"
 curl \
   --output "$HOME/.git_bash_prompt.sh" \
   "https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-prompt.sh" \
    > /dev/null 2>&1
 
-echo 'Download the Git Completion script from the official Git Repo to ~/.git_bash_autocomplete.sh'
-echo 'When in a Git directory, this adds auto complete for Git Branches.'
+info 'Download the Git Completion script from the official Git Repo to ~/.git_bash_autocomplete.sh'
+info 'When in a Git directory, this adds auto complete for Git Branches.'
+
+if [ -f "$HOME/.git_bash_autocomplete.sh" ]; then
+	mv "$HOME/.git_bash_autocomplete.sh" "$HOME/.git_bash_autocomplete.sh.old"
+fi
 
 touch "$HOME/.git_bash_autocomplete.sh"
-cp "$HOME/.git_bash_autocomplete.sh" "$HOME/.git_bash_autocomplete.sh.old"
 curl \
   --output "$HOME/.git_bash_autocomplete.sh" \
   "https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-completion.bash" \
--- dev/git/git-setup.sh
+++ dev/git/git-setup.sh
@@ -10,17 +10,18 @@ cd "$REPO"
 source "$REPO/bin/.helper.sh"
 OS="$(os)"
 
-# Setup Git Bash features
+info "Setup Git Bash features"
 
 bash "$REPO/dev/git/git-bash-setup.sh"
 
-# Set global Git Config
+info "Set global Git Config"
 
 git config --global init.defaultBranch "develop"
 git config --global push.default "simple"
 git config --global push.followtags "false"
 git config --global push.autosetupremote "true"
 git config --global merge.ff "only"
+git config --global pull.ff "only"
 
 git config --global core.whitespace "fix,-indent-with-non-tab,trailing-space,cr-at-eol"
 git config --global core.ignorecase "false"
@@ -42,7 +43,7 @@ git config --global color.status.added "green"
 git config --global color.status.changed "yellow"
 git config --global color.status.untracked "red"
 
-# Add Git aliases
+info "Add Git aliases"
 
 git config --global alias.f "fetch"
 git config --global alias.st "status --untracked-files=all --short --branch"
@@ -53,3 +54,19 @@ git config --global alias.br "branch --show-current"
 git config --global alias.cbr "checkout -b"
 git config --global alias.lg "log --pretty=format:'%Cblue%h%Creset %s %Cgreen%an, %cr %Creset' --abbrev-commit --date=relative"
 git config --global alias.graph "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+
+if [ -n "$SSH_PUBLIC_KEY_PATH" ]; then
+	info "Configuring commit signing for '$SSH_PUBLIC_KEY_PATH'"
+
+	touch $HOME/.ssh/allowed_signers
+
+	git config --global gpg.format=ssh
+	git config --global commit.gpgsign=true
+	git config --global user.signingkey=$SSH_PUBLIC_KEY_PATH
+	git config --global credential.helper=store
+	git config --global gpg.ssh.allowedsignersfile=$HOME/.ssh/allowed_signers
+
+	echo "$(git config --get user.email) namespaces=\"git\" $(cat $SSH_PUBLIC_KEY_PATH)" >> $HOME/.ssh/allowed_signers
+else
+	warn "Skipping commit signing. Requires 'SSH_PUBLIC_KEY_PATH' var to be exported when running this script."
+fi
