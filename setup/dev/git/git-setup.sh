02f78ca6c7981766eef1da82dad2f366e4be22de
Brendan Murty <brendan@bcm.works>
2026-07-23 09:10:15 +1000

    Update Git config to also get tags when fetching or pulling

--- setup/dev/git/git-setup.sh
+++ setup/dev/git/git-setup.sh
@@ -24,6 +24,7 @@ git config --global merge.ff "only"
 git config --global pull.ff "only"
 git config --global merge.commit no
 git config --global merge.ff no
+git config --global remote.origin.tagOpt --tags
 
 git config --global core.whitespace "fix,-indent-with-non-tab,trailing-space,cr-at-eol"
 git config --global core.ignorecase "false"
