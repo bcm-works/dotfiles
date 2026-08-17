9571e1ea07ceff81ec9a7892442a9294e7da0968
Brendan Murty <brendan@bcm.works>
2026-08-17 15:33:19 +1000

    Improve Go install process

--- dev/go-setup.sh
+++ dev/go-setup.sh
@@ -3,6 +3,7 @@
 #
 # Go setup
 # 	- More info at https://go.dev/
+# 	- Install commands from https://go.dev/doc/install
 #   - To run this script:
 #     - Clone this repo
 #     - Open that dir in a new terminal
@@ -16,17 +17,33 @@ REPO="$(dir_repo)"
 OS="$(os)"
 cd "$REPO"
 
+GO_PATH="$HOME/Code"
+GO_DOWNLOAD="$HOME/Downloads/go1.26.6.linux-amd64.tar.gz"
+
 if [ ! "$(command -v go)" ]; then
-	if [[ "$(os_debian_based)" ]]; then
-	  info 'Installing Go from APT'
-	  sudo apt -qq --assume-yes install golang-go > /dev/null 2>&1
-	else
+	if [[ "$OS" == "macOS" || "$OS" == "Windows" ]]; then
 		error 'Please install Go manually first - https://go.dev/doc/install'
 	  exit 1
+	else
+		warn 'Requesting sudo'
+		sudo -v
+
+		info 'Downloading and installing Go version 1.26.6'
+
+		rm -rf "$GO_DOWNLOAD"
+		curl --silent --show-error \
+			--output "$GO_DOWNLOAD" \
+			"https://dl.google.com/go/go1.26.6.linux-amd64.tar.gz"
+
+		sudo rm -rf /usr/local/go
+		sudo tar -C /usr/local -xzf "$GO_DOWNLOAD"
+
+		export PATH=$PATH:/usr/local/go/bin
 	fi
 fi
 
-GO_PATH="$HOME/Code"
+warn 'Reloading shell to apply changes'
+source "$HOME/.bashrc"
 
 info "Setting GOPATH to $GO_PATH"
 mkdir -p "$GO_PATH"
@@ -36,13 +53,9 @@ info 'Configuring Go'
 go env -w GOPROXY="https://proxy.golang.org,direct"
 go env -w GOSUMDB="sum.golang.org"
 
-info 'Installing Go version 1.26.6'
-go install golang.org/dl/go1.26.6@latest > /dev/null 2>&1
-go1.26.6 download > /dev/null 2>&1
-
-warn 'Reloading shell to apply changes'
-source "$HOME/.bashrc"
-
 success "Go installed"
-info "Version: $(go version)"
-info "Location: $(which go)"
+
+info "Go Version: $(go version)"
+info "Go Binary: $(which go)"
+info "Go Root: $(go env GOROOT)"
+info "Go Path: $(go env GOPATH)"
