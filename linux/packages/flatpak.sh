57a4ad798296cf930583d7fa273936dd17c12be7
Brendan Murty <brendan@bcm.works>
2026-09-02 12:51:27 +1000

    Simplify Flatpak batch installs from package list file

--- linux/packages/flatpak.sh
+++ linux/packages/flatpak.sh
@@ -23,20 +23,12 @@ if ! command -v flatpak &> /dev/null; then
 fi
 
 if [[ -f "$PACKAGES_LIST" ]]; then
-	info "Installing Flatpak apps from '$PACKAGES_LIST'"
+	info "Processing Flatpak apps from '$PACKAGES_LIST'"
 
-	while IFS= read -r line; do
-		IS_INSTALLED="$(flatpak list --columns=application | grep -q "^$line$" && echo "true")"
-		if [[ "$IS_INSTALLED" != "true" ]]; then
-    		info "  $line - Installing"
-    		flatpak install --assumeyes --or-update "$line" > /dev/null 2>&1
-    	else
-    		success "  $line - Installed, skipping"
-    	fi
+	while read -r package; do
+		info "  Installing $package"
+    flatpak install --assumeyes --or-update "$package" > /dev/null 2>&1
 	done < "$PACKAGES_LIST"
-
-	warn 'Flatpak setup completed, a system reboot is recommended.'
 else
 	warn "Skipping Flatpak app installs, file not found at '$PACKAGES_LIST'"
 fi
-
